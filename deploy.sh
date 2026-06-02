8798d2ddb40ce44828594edcce8ffbc0d96b8517
Evan Loria <evan.loria4@gmail.com>
2026-06-02 10:45:11 -0500

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Pulling latest changes..."
+git pull origin main
+
+echo "Installing dependencies..."
+npm install
+
+echo "Building..."
+npm run build
+
+echo "Restarting PM2..."
+pm2 restart sawa
+
+echo "Done."
